128d327f3336afdd53c02a08761afeaf50e3e18e
chemin <chemin0428@gmail.com>
2022-06-23 13:22:03 +0900

    react-hook 연습

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -34,7 +34,7 @@ function ToDoList() {
 const Register = styled.input`
 	width: 200px;
 	height: 30px;
-	margin: auto;
+	/* margin: auto; */
 `;
 
 const AddButton = styled.button`
@@ -43,23 +43,67 @@ const AddButton = styled.button`
 	background-color: green;
 	border-radius: 10px;
 	border: 0;
-	margin: auto;
+	/* margin: auto; */
 `;
 
+interface IForm {
+	email: string;
+	firstname: string;
+	lastname: string;
+	username: string;
+	password: string;
+	password1: string;
+	extraError?: string;
+}
+
 function ToDoList() {
-	const { register, handleSubmit, formState } = useForm();
-	const onValid = (data: any) => {
-		console.log(data);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		setError
+	} = useForm<IForm>({
+		defaultValues: {
+			email: '@naver.com'
+		}
+	});
+
+	const onValid = (data: IForm) => {
+		if (data.password !== data.password1) {
+			setError('password1', { message: 'Password are not the same' }, { shouldFocus: true });
+		}
+		setError('extraError', { message: 'Server offline.' });
 	};
-	console.log(formState.errors);
+
+	console.log(errors);
 	return (
 		<div>
 			<form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit(onValid)}>
-				<Register {...register('email', { required: true })} placeholder="Email" />
-				<Register {...register('firstName', { required: true })} placeholder="First Name" />
-				<Register {...register('lastName', { required: true })} placeholder="Last Name" />
-				<Register {...register('username', { required: true, minLength: 10 })} placeholder="Username" />
-				<Register {...register('password', { required: true, minLength: 5 })} placeholder="Password" />
+				<Register
+					{...register('email', {
+						required: 'Email is required',
+						pattern: { value: /^[A-Za-z0-9._%+-]+@naver.com$/, message: 'Only naver.com emails allowed' }
+					})}
+					placeholder="Email"
+				/>
+				<span>{errors?.email?.message}</span>
+				<Register
+					{...register('firstname', {
+						required: 'Write here',
+						validate: {
+							noNico: value => (value.includes('nico') ? 'no nicos allowed' : true),
+							noNick: value => (value.includes('nick') ? 'no nick allowed' : true)
+						}
+					})}
+					placeholder="First Name"
+				/>
+				<span>{errors?.firstname?.message}</span>
+				<Register {...register('lastname', { required: 'Write here' })} placeholder="Last Name" />
+				<span>{errors?.lastname?.message}</span>
+				<Register {...register('username', { required: 'Write here', minLength: 10 })} placeholder="Username" />
+				<span>{errors?.username?.message}</span>
+				<Register {...register('password', { required: 'Write here', minLength: 5 })} placeholder="Password" />
+				<span>{errors?.password?.message}</span>
 				<Register
 					{...register('password1', {
 						required: 'Password is required',
@@ -70,7 +114,9 @@ function ToDoList() {
 					})}
 					placeholder="Password1"
 				/>
+				<span>{errors?.password1?.message}</span>
 				<AddButton>Add</AddButton>
+				<span>{errors?.extraError?.message}</span>
 			</form>
 		</div>
 	);
